c4d501baf8e9cd602945083f84cd8985ae2f0b99
Olivier Chaussavoine <olivier.chaussavoine@gmail.com>
2014-03-23 11:17:20 +0100

    corrections

--- src/test/py/run.py
+++ src/test/py/run.py
@@ -50,7 +50,7 @@ def tests():
     molet.mkdir(os.path.join(curdir,'results'),ignoreWarning = True)
     molet.mkdir(os.path.join(curdir,'expected'),ignoreWarning = True)
     try:
-        wait_for_true(0.1,"SELECT value=102,value FROM market.tvar WHERE name='OC_CURRENT_PHASE'",
+        utilt.wait_for_true(srvob_conf.dbBO,0.1,"SELECT value=102,value FROM market.tvar WHERE name='OC_CURRENT_PHASE'",
             msg="Waiting for market opening")
     except psycopg2.OperationalError,e:
         print "Please adjust DB_NAME,DB_USER,DB_PWD,DB_PORT parameters of the file src/test/py/srv_conf.py"
@@ -71,7 +71,7 @@ def tests():
 
     _tok,_terr,_num_test = 0,0,0
 
-    prtest.title('running regression tests on %s' % (srvob_conf.DB_NAME,))
+    prtest.title('running tests on database "%s"' % (srvob_conf.DB_NAME,))
     #print '='*PARLEN
     
     
@@ -93,19 +93,21 @@ def tests():
             exec_script(cur,'reset_market.sql',None)
             exec_script(cur,file_test,f)
 
-            wait_for_true(20,"SELECT market.fstackdone()")
+            utilt.wait_for_true(srvob_conf.dbBO,20,"SELECT market.fstackdone()")
+
+            dump = utilt.Dumper(srvob_conf.dbBO,options)
 
             conn = molet.DbData(srvob_conf.dbBO)
             try:
                 with molet.DbCursor(conn) as cur:
-                    dump_torder(cur,f)
-                    dump_tmsg(cur,f)
+                    dump.torder(cur,f)
+                    dump.tmsg(cur,f)
 
             finally:
                 conn.close()
 
         if(os.path.exists(_fre)):
-            if(files_clones(_fte,_fre)):
+            if(utilt.files_clones(_fte,_fre)):
                 _tok +=1
                 _terr -=1
                 print '%i\tY\t%s\t%s' % (_num_test,_nom_test,titre_test)
@@ -161,14 +163,17 @@ def exec_script(cur,fn,fdr):
     return 
 
 def execinst(cur_login,sql,fdr,cursor):
+    global options
+
     if cur_login == 'admin':
         cur_login = None
     conn = molet.DbData(srvob_conf.dbBO,login = cur_login)
+    dump = utilt.Dumper(srvob_conf.dbBO,options)
     try:
         with molet.DbCursor(conn,exit = True) as _cur:
             _cur.execute(sql)
             if fdr:
-                dump_cur(_cur,fdr)
+                dump.cur(_cur,fdr)
     finally:
         conn.close()
 '''
@@ -179,101 +184,6 @@ yorder not shown:
     carre_prov box -- carre_prov @> pos_requ 
 '''
 
-def dump_torder(cur,fdr):
-    fdr.write(SEPARATOR)
-    fdr.write('table: torder\n')
-    cur.execute('SELECT * FROM market.vord order by id asc')
-    dump_cur(cur,fdr)
-
-def dump_cur(cur,fdr,_len=10):
-    if(cur is None): return
-    cols = [e.name for e in cur.description]
-    row_format = ('{:>'+str(_len)+'}')*len(cols)
-    fdr.write(row_format.format(*cols)+'\n')
-    fdr.write(row_format.format(*(['+'+'-'*(_len-1)]*len(cols)))+'\n')
-    for res in cur:
-        fdr.write(row_format.format(*res)+'\n')
-    return
-
-import json
-
-def dump_tmsg(cur,fdr):
-    fdr.write(SEPARATOR)
-    fdr.write('table: tmsg')
-    fdr.write(SEPARATOR)
-    
-    cur.execute('SELECT id,typ,usr,jso FROM market.tmsg  order by id asc')
-    for res in cur:
-        _id,typ,usr,jso = res
-        _jso = json.loads(jso)
-        if typ == 'response':
-            if _jso['error']['code']==None:
-                _msg = 'Primitive id:%i from %s: OK\n' % (_jso['id'],usr)
-            else:
-                _msg = 'Primitive id:%i from %s: ERROR(%i,%s)\n' % (usr,_jso['id'],
-                        _jso['error']['code'],_jso['error']['reason'])
-        elif typ == 'exchange':
-
-            _fmt = '''Cycle id:%i Exchange id:%i for %s @%s:
-            \t%i:mvt_from %s @%s : %i \'%s\' -> %s @%s
-            \t%i:mvt_to   %s @%s : %i \'%s\' <- %s @%s 
-            \tstock id:%i remaining after exchange: %i \'%s\' \n''' 
-            _dat = (
-                _jso['cycle'],_jso['id'],_jso['stock']['own'],usr,
-                _jso['mvt_from']['id'],_jso['stock']['own'],usr,_jso['mvt_from']['qtt'],_jso['mvt_from']['nat'],_jso['mvt_from']['own'],_jso['mvt_from']['usr'],
-                _jso['mvt_to']['id'], _jso['stock']['own'],usr,_jso['mvt_to']['qtt'],_jso['mvt_to']['nat'],_jso['mvt_to']['own'],_jso['mvt_to']['usr'],
-                _jso['stock']['id'],_jso['stock']['qtt'],_jso['stock']['nat'])
-            _msg = _fmt %_dat
-        else:
-            _msg = str(res)
-
-        fdr.write('\t%i:'%_id+_msg+'\n') 
-        if options.verbose:
-            print jso
-    return
-
-import filecmp
-def files_clones(f1,f2):
-    #res = filecmp.cmp(f1,f2)
-    return (md5sum(f1) == md5sum(f2))
-
-import hashlib
-def md5sum(filename, blocksize=65536):
-    hash = hashlib.md5()
-    with open(filename, "r+b") as f:
-        for block in iter(lambda: f.read(blocksize), ""):
-            hash.update(block)
-    return hash.hexdigest()
-
-def wait_for_true(delai,sql,msg=None):
-    _i = 0;
-    _w = 0;
-
-    while True:
-        _i +=1
-
-        conn = molet.DbData(srvob_conf.dbBO)
-        try:
-            with molet.DbCursor(conn) as cur:
-                cur.execute(sql)
-                r = cur.fetchone()
-                # print r
-                if r[0] == True:
-                    break
-        finally:
-            conn.close()
-
-        if msg is None:
-            pass
-        elif(_i%10)==0:
-            print msg
-
-        _a = 0.1;
-        _w += _a;
-
-        if _w > delai: # seconds
-            raise ValueError('After %f seconds, %s != True' % (_w,sql))
-        time.sleep(_a)
 
 '''---------------------------------------------------------------------------
 arguments
--- src/test/py/utilt.py
+++ src/test/py/utilt.py
@@ -2,22 +2,132 @@
 import string
 
 class PrTest(object):
+    ''' results printing '''
+    def __init__(self,parlen,sep):
+        self.parlen = parlen+ parlen%2
+        self.sep = sep
 
-	def __init__(self,parlen,sep):
-		self.parlen = parlen+ parlen%2
-		self.sep = sep
+    def title(self,title):
+        _l = len(title)
+        _p = max(_l%2 +_l,40)
+        _x = self.parlen -_p
+        if (_x > 2):
+            print (_x/2)*self.sep + string.center(title,_p) + (_x/2)*self.sep
+        else:
+            print string.center(text,self.parlen)
 
-	def title(self,title):
-		_l = len(title)
-		_p = max(_l%2 +_l,40)
-		_x = self.parlen -_p
-		if (_x > 2):
-			print (_x/2)*self.sep + string.center(title,_p) + (_x/2)*self.sep
-		else:
-			print string.center(text,self.parlen)
+    def line(self):
+        print self.parlen*self.sep
 
-	def line(self):
-		print self.parlen*self.sep
+    def center(self,text):
+        print string.center(text,self.parlen)
 
-	def center(self,text):
-		print string.center(text,self.parlen)
+############################################################################
+''' file comparison '''
+import filecmp
+def files_clones(f1,f2):
+    #res = filecmp.cmp(f1,f2)
+    return (md5sum(f1) == md5sum(f2))
+
+import hashlib
+def md5sum(filename, blocksize=65536):
+    hash = hashlib.md5()
+    with open(filename, "r+b") as f:
+        for block in iter(lambda: f.read(blocksize), ""):
+            hash.update(block)
+    return hash.hexdigest()
+
+############################################################################
+SEPARATOR = '\n'+'-'*80 +'\n'
+import json
+
+class Dumper(object):
+
+    def __init__(self,conf,options):
+        self.options =options
+        self.conf = conf
+
+    def torder(self,cur,fdr):
+        fdr.write(SEPARATOR)
+        fdr.write('table: torder\n')
+        cur.execute('SELECT * FROM market.vord order by id asc')
+        self.cur(cur,fdr)
+        return
+
+    def cur(self,cur,fdr,_len=10):
+        if(cur is None): return
+        cols = [e.name for e in cur.description]
+        row_format = ('{:>'+str(_len)+'}')*len(cols)
+        fdr.write(row_format.format(*cols)+'\n')
+        fdr.write(row_format.format(*(['+'+'-'*(_len-1)]*len(cols)))+'\n')
+        for res in cur:
+            fdr.write(row_format.format(*res)+'\n')
+        return
+
+    def tmsg(self,cur,fdr):
+        fdr.write(SEPARATOR)
+        fdr.write('table: tmsg')
+        fdr.write(SEPARATOR)
+        
+        cur.execute('SELECT id,typ,usr,jso FROM market.tmsg  order by id asc')
+        for res in cur:
+            _id,typ,usr,jso = res
+            _jso = json.loads(jso)
+            if typ == 'response':
+                if _jso['error']['code']==None:
+                    _msg = 'Primitive id:%i from %s: OK\n' % (_jso['id'],usr)
+                else:
+                    _msg = 'Primitive id:%i from %s: ERROR(%i,%s)\n' % (usr,_jso['id'],
+                            _jso['error']['code'],_jso['error']['reason'])
+            elif typ == 'exchange':
+
+                _fmt = '''Cycle id:%i Exchange id:%i for %s @%s:
+            \t%i:mvt_from %s @%s : %i \'%s\' -> %s @%s
+            \t%i:mvt_to   %s @%s : %i \'%s\' <- %s @%s 
+            \tstock id:%i remaining after exchange: %i \'%s\' \n''' 
+                _dat = (
+                    _jso['cycle'],_jso['id'],_jso['stock']['own'],usr,
+                    _jso['mvt_from']['id'],_jso['stock']['own'],usr,_jso['mvt_from']['qtt'],_jso['mvt_from']['nat'],_jso['mvt_from']['own'],_jso['mvt_from']['usr'],
+                    _jso['mvt_to']['id'], _jso['stock']['own'],usr,_jso['mvt_to']['qtt'],_jso['mvt_to']['nat'],_jso['mvt_to']['own'],_jso['mvt_to']['usr'],
+                    _jso['stock']['id'],_jso['stock']['qtt'],_jso['stock']['nat'])
+                _msg = _fmt %_dat
+            else:
+                _msg = str(res)
+
+            fdr.write('\t%i:'%_id+_msg+'\n') 
+            if self.options.verbose:
+                print jso
+        return
+
+############################################################################
+import molet
+import time
+def wait_for_true(conf,delai,sql,msg=None):
+    _i = 0;
+    _w = 0;
+
+    while True:
+        _i +=1
+
+        conn = molet.DbData(conf)
+        try:
+            with molet.DbCursor(conn) as cur:
+                cur.execute(sql)
+                r = cur.fetchone()
+                # print r
+                if r[0] == True:
+                    break
+        finally:
+            conn.close()
+
+        if msg is None:
+            pass
+        elif(_i%10)==0:
+            print msg
+
+        _a = 0.1;
+        _w += _a;
+
+        if _w > delai: # seconds
+            raise ValueError('After %f seconds, %s != True' % (_w,sql))
+        time.sleep(_a)
